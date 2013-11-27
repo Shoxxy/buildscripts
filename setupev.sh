0df901a09b4f2b3bc415e2c6cfb9f4513e8fac4d
Hrubak <hrubak@gmail.com>
2013-11-27 09:27:35 -0500

    Update setupev.sh

--- setupev.sh
+++ setupev.sh
@@ -211,7 +211,7 @@ prepare_environment()
         fi
         
         export PATH=~/bin:$PATH
-        curl https://dl-ssl.google.com/dl/googlesource/git-repo/repo > ~/bin/repo
+        curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
         chmod a+x ~/bin/repo
         source ~/.profile
         repo selfupdate
